a1ff49d50ad10a7bc9241905d001d938e11edc04
masyuko0222 <masyuko0222@gmail.com>
2023-08-15 22:49:23 +0900

    リファクタリング

--- 02.calendar/calender.js
+++ 02.calendar/calender.js
@@ -27,25 +27,29 @@ function printDaysOfWeek() {
 }
 
 function printDates() {
-  const LAST_DATE = DAYJS.daysInMonth();
-  let tmp_dayjs = DAYJS;
+  fillBlanks(DAYJS);
 
-  let tmp_arry = new Array(tmp_dayjs.day());
-  let aaa = [...tmp_arry.fill('  ')].join(' ') + ' ';
-  process.stdout.write(aaa);
+  const LAST_DATE = DAYJS.daysInMonth();
+  let current_dayjs = DAYJS;
   let week = [];
 
   for (let i = 1; i <= LAST_DATE; i++) {
-    week.push(tmp_dayjs.date().toString().padStart(2));
+    week.push(DAYJS.date().toString().padStart(2));
 
-    if (tmp_dayjs.day() === 6 || i === LAST_DATE){
+    if (current_dayjs.day() === 6 || i === LAST_DATE){
       process.stdout.write(week.join(' '));
       process.stdout.write(`\n`);
-      week.splice(0);
+      week.splice(0); // array reset
     }
-
-    tmp_dayjs = tmp_dayjs.add(1, "day");
+    current_dayjs = current_dayjs.add(1, "day");
   }
+}
+
+function fillBlanks(dayJsObject) {
+  const blanksCount = dayJsObject.day();
+
+  let blanksArray = new Array(blanksCount).fill('  ');
+  process.stdout.write(blanksArray.join(' ') + ' ');
 }
 
 main();
