561b90baa9b3e3f2dbbbbb56311f788ede60fff3
masyuko0222 <masyuko0222@gmail.com>
2023-08-15 21:59:07 +0900

    仕様を満たすように。変数名とかリファクタリングはまったくなので要修正

--- 02.calendar/calender.js
+++ 02.calendar/calender.js
@@ -30,13 +30,20 @@ function printDates() {
   const LAST_DATE = DAYJS.daysInMonth();
   let tmp_dayjs = DAYJS;
 
+  let tmp_arry = new Array(tmp_dayjs.day());
+  let aaa = [...tmp_arry.fill('  ')].join(' ') + ' ';
+  process.stdout.write(aaa);
+  let week = [];
+
   for (let i = 1; i <= LAST_DATE; i++) {
-    let date = `${tmp_dayjs.date()}`;
-    let day_of_week = tmp_dayjs.day();
-    process.stdout.write(date);
-    if (day_of_week === 6) {
+    week.push(tmp_dayjs.date().toString().padStart(2));
+
+    if (tmp_dayjs.day() === 6 || i === LAST_DATE){
+      process.stdout.write(week.join(' '));
       process.stdout.write(`\n`);
+      week.splice(0);
     }
+
     tmp_dayjs = tmp_dayjs.add(1, "day");
   }
 }
