14c427a3fa376caeb3c8fba671ec6a765f42196b
masyuko0222 <masyuko0222@gmail.com>
2023-08-15 19:24:44 +0900

    土曜日で改行しつつ日付を表示するように

--- 02.calendar/calender.js
+++ 02.calendar/calender.js
@@ -10,12 +10,12 @@ const ARGS = minimist(process.argv);
 const YEAR = ARGS.y ?? dayjs().year();
 const MONTH = ARGS.m ? ARGS.m - 1 : dayjs().month();
 
-const DAYJS = dayjs({ year: YEAR, month: MONTH });
+let DAYJS = dayjs({ year: YEAR, month: MONTH });
 
 function main() {
   printMonthYear(DAYJS.month(), DAYJS.year());
   printDaysOfWeek();
-  //  ptintDates();
+  printDates();
 }
 
 function printMonthYear(month, year) {
@@ -24,6 +24,21 @@ function printMonthYear(month, year) {
 
 function printDaysOfWeek() {
   process.stdout.write("日 月 火 水 木 金 土\n");
+}
+
+function printDates() {
+  const LAST_DATE = DAYJS.daysInMonth();
+  let tmp_dayjs = DAYJS;
+
+  for (let i = 1; i <= LAST_DATE; i++) {
+    let date = `${tmp_dayjs.date()}`;
+    let day_of_week = tmp_dayjs.day();
+    process.stdout.write(date);
+    if (day_of_week === 6) {
+      process.stdout.write(`\n`);
+    }
+    tmp_dayjs = tmp_dayjs.add(1, "day");
+  }
 }
 
 main();
