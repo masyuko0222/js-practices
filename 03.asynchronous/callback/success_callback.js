784025be34f5a1fd7ce824e5ec165d37665465bf
masyuko0222 <masyuko0222@gmail.com>
2023-08-21 22:41:06 +0900

    処理には関係ない、出力する文字列の微修正

--- 03.asynchronous/callback/success_callback.js
+++ 03.asynchronous/callback/success_callback.js
@@ -7,18 +7,18 @@ const db = new sqlite3.Database(":memory:");
 db.run(
   `CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`,
   () => {
-    console.log("Created table successfully.");
+    console.log("Created books table successfully.");
     db.run(
       `INSERT INTO books(title) VALUES($title1), ($title2)`,
       { $title1: "First Book", $title2: "Second Book" },
       function () {
-        console.log("Inserted 2 records successfully.");
+        console.log("Inserted records successfully.");
         console.log(`Last inserted ID is ${this.lastID}.`);
         db.all(`SELECT * FROM books`, (_, row) => {
-          console.log("Selected all records.");
+          console.log("Selected all records successfully.");
           console.log(row);
           db.close(() => {
-            console.log("Closed DB");
+            console.log("Closed DB successfully.");
           });
         });
       }
