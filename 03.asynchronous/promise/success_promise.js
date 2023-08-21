059c2fcbf12278e9e65e64fd3574d3a8c155ecf5
masyuko0222 <masyuko0222@gmail.com>
2023-08-21 22:37:51 +0900

    Promise成功パターン

---
+++ 03.asynchronous/promise/success_promise.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+"use strict";
+
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database(":memory:");
+
+new Promise((resolve) => {
+  db.run(
+    `CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`,
+    () => {
+      console.log("Created books table successfully.");
+      resolve();
+    }
+  );
+})
+  .then(() => {
+    return new Promise((resolve) => {
+      db.run(
+        `INSERT INTO books (title) VALUES ($title1), ($title2)`,
+        { $title1: "First Books", $title2: "Second Book" },
+        function () {
+          console.log("Inserted records successfully.");
+          console.log(`The last inserted ID is ${this.lastID}`);
+          resolve();
+        }
+      );
+    });
+  })
+  .then(() => {
+    return new Promise((resolve) => {
+      db.all(`SELECT * FROM books`, (_, row) => {
+        console.log("Selected all records successfully.");
+        console.log(row);
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    db.close(() => {
+      console.log("Closed DB successfully.");
+    });
+  });
