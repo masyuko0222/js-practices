e3090dbb171518cf61c83a49bd87df6f5d947da1
masyuko0222 <masyuko0222@gmail.com>
2023-08-22 21:13:22 +0900

    不要なコメントアウトを削除

--- 03.asynchronous/async/success_async.js
+++ 03.asynchronous/async/success_async.js
@@ -4,37 +4,6 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
-//new Promise((resolve) => {
-//  db.run(
-//    `CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)`,
-//    () => {
-//      console.log("Created books table successfully.");
-//      resolve();
-//    }
-//  );
-//})
-//  .then(() => {
-//    db.run(
-//      `INSERT INTO books (title) VALUES ($title1), ($title2)`,
-//      { $title1: "First Books", $title2: "Second Book" },
-//      function () {
-//        console.log("Inserted records successfully.");
-//        console.log(`The last inserted ID is ${this.lastID}`);
-//      }
-//    );
-//  })
-//  .then(() => {
-//    db.all(`SELECT * FROM books`, (_, row) => {
-//      console.log("Selected all records successfully.");
-//      console.log(row);
-//    });
-//  })
-//  .finally(() => {
-//    db.close(() => {
-//      console.log("Closed DB successfully.");
-//    });
-//  });
-
 async function asyncMain() {
   await createTablePromise();
   console.log("Created books table successfully.");
