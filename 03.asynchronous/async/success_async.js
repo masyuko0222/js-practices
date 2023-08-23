73551b95bfe27d987fcd2a8acea715e91a448b77
masyuko0222 <masyuko0222@gmail.com>
2023-08-23 23:20:23 +0900

    await成功パターン

--- 03.asynchronous/async/success_async.js
+++ 03.asynchronous/async/success_async.js
@@ -4,26 +4,10 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-async function asyncMain() {
-  await createTablePromise();
-  console.log("Created books table successfully.");
-
-  const lastId = await insertRecordsPromise();
-  console.log("Inserted records successfully.");
-  console.log(`The last inserted ID is ${lastId}.`);
-
-  const allRecords = await selectRecordsPromise();
-  console.log("Selected all records successfully.");
-  console.log(allRecords);
-
-  await closeDbPromise();
-  console.log("Closed DB successfully.");
-}
-
 const createTablePromise = () => {
   return new Promise((resolve) => {
     db.run(
-      "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+      "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
       () => {
         resolve();
       }
@@ -35,7 +19,7 @@ const insertRecordsPromise = () => {
   return new Promise((resolve) => {
     db.run(
       "INSERT INTO books (title) VALUES ($title1), ($title2)",
-      { $title1: "First book", $title2: "Second Book" },
+      { $title1: "FirstBook", $title2: "SecondBook" },
       function () {
         resolve(this.lastID);
       }
@@ -43,10 +27,10 @@ const insertRecordsPromise = () => {
   });
 };
 
-const selectRecordsPromise = () => {
+const selectAllRecordsPromise = () => {
   return new Promise((resolve) => {
-    db.all("SELECT * FROM books", (_, row) => {
-      resolve(row);
+    db.all("SELECT * FROM books", (_, rows) => {
+      resolve(rows);
     });
   });
 };
@@ -59,4 +43,20 @@ const closeDbPromise = () => {
   });
 };
 
-asyncMain();
+async function main() {
+  await createTablePromise();
+  console.log("Created books table successfully.");
+
+  const lastID = await insertRecordsPromise();
+  console.log("Inserted records successfully.");
+  console.log(`The inserted last ID is ${lastID}`);
+
+  const rows = await selectAllRecordsPromise();
+  console.log("The below allay means all records.");
+  console.log(rows);
+
+  await closeDbPromise();
+  console.log("Closed DB successfully.");
+}
+
+main();
