378da609b68627f2c9bab451d17e6da0f183b10f
masyuko0222 <masyuko0222@gmail.com>
2023-08-23 22:33:16 +0900

    Promise成功パターン

--- 03.asynchronous/promise/success_promise.js
+++ 03.asynchronous/promise/success_promise.js
@@ -4,30 +4,50 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-new Promise((resolve) => {
-  db.run(
-    "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    () => {
-      console.log("Created books table successfully.");
-      resolve();
-    }
-  );
-})
-  .then(() => {
+const createTablePromise = () => {
+  return new Promise((resolve) => {
     db.run(
-      "INSERT INTO books (title) VALUES ($title1), ($title2)",
-      { $title1: "First Books", $title2: "Second Book" },
-      function () {
-        console.log("Inserted records successfully.");
-        console.log(`The last inserted ID is ${this.lastID}`);
+      "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+      () => {
+        resolve();
       }
     );
-  })
-  .then(() => {
-    db.all("SELECT * FROM books", (_, row) => {
-      console.log("Selected all records successfully.");
-      console.log(row);
+  });
+};
+
+const insertRecordsPromise = () => {
+  return new Promise((resolve) => {
+    db.run(
+      "INSERT INTO books (title) VALUES ($title1), ($title2)",
+      { $title1: "FirstBook", $title2: "SecondBook" },
+      function () {
+        resolve(this.lastID);
+      }
+    );
+  });
+};
+
+const selectAllRecordsPromise = () => {
+  return new Promise((resolve) => {
+    db.all("SELECT * FROM books", (_, rows) => {
+      resolve(rows);
     });
+  });
+};
+
+createTablePromise()
+  .then(() => {
+    console.log("Created books table successfully.");
+    return insertRecordsPromise();
+  })
+  .then((lastId) => {
+    console.log("Inserted records successfully.");
+    console.log(`The inserted last ID is ${lastId}`);
+    return selectAllRecordsPromise();
+  })
+  .then((rows) => {
+    console.log("The below allay means all records.");
+    console.log(rows);
   })
   .finally(() => {
     db.close(() => {
