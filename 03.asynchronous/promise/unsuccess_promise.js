5561c807daf497865cb32fedbd1e72cd47ebfec7
masyuko0222 <masyuko0222@gmail.com>
2023-08-23 23:03:38 +0900

    Promise失敗パターン

--- 03.asynchronous/promise/unsuccess_promise.js
+++ 03.asynchronous/promise/unsuccess_promise.js
@@ -4,41 +4,57 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-new Promise((resolve) => {
-  db.run(
-    "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    () => {
-      console.log("Created books table successfully.");
-      resolve();
-    }
-  );
-})
-  .then(() => {
-    return new Promise((_, reject) => {
-      db.run(
-        "INSERT INTO books (no_column) VALUES ($title1), ($title2)",
-        { $title1: "First Books", $title2: "Second Book" },
-        (err) => {
-          console.log("Failed inserting");
-          console.log(err.message);
-          reject();
+const createTablePromise = () => {
+  return new Promise((resolve) => {
+    db.run(
+      "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+      () => {
+        resolve();
+      }
+    );
+  });
+};
+
+const insertRecordsRejectPromise = () => {
+  return new Promise((_, reject) => {
+    db.run(
+      "INSERT INTO books (no_column) VALUES ($title1), ($title2)",
+      { $title1: "FirstBook", $title2: "SecondBook" },
+      (error) => {
+        if (error) {
+          reject(error);
         }
-      );
+      }
+    );
+  });
+};
+
+const selectAllRecordsRejectPromise = () => {
+  return new Promise((_, reject) => {
+    db.all("SELECT * FROM no_table", (error) => {
+      if (error) {
+        reject(error);
+      }
     });
+  });
+};
+
+createTablePromise()
+  .then(() => {
+    console.log("Created books table successfully.");
+    return insertRecordsRejectPromise();
   })
-  .catch(() => {
-    return new Promise((_, reject) => {
-      db.all("SELECT * FROM no_table", (err) => {
-        console.log("Failed selecting.");
-        console.log(err.message);
-        reject();
-      });
-    });
+  .catch((error) => {
+    console.log("Error occured!!!");
+    console.error(error.message);
+    return selectAllRecordsRejectPromise();
+  })
+  .catch((error) => {
+    console.log("Error occured!");
+    console.error(error.message);
   })
   .finally(() => {
-    return new Promise(() => {
-      db.close(() => {
-        console.log("Closed DB successfully.");
-      });
+    db.close(() => {
+      console.log("Closed DB successfully.");
     });
   });
