f341ac854bc58aa1a80823fc4861a11cb68865c4
masyuko0222 <masyuko0222@gmail.com>
2023-08-25 23:49:46 +0900

    Await OK版

--- 03.asynchronous/async/success_async.js
+++ 03.asynchronous/async/success_async.js
@@ -4,58 +4,49 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-const createTablePromise = () => {
+const dbRunPromise = (sql, param = {}) => {
   return new Promise((resolve) => {
-    db.run(
-      "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-      () => {
-        resolve();
-      }
-    );
+    db.run(sql, param, function () {
+      resolve(this.lastID);
+    });
   });
 };
 
-const insertRecordsPromise = () => {
+const dbAllPromise = (sql, param = {}) => {
   return new Promise((resolve) => {
-    db.run(
-      "INSERT INTO books (title) VALUES ($title1), ($title2)",
-      { $title1: "FirstBook", $title2: "SecondBook" },
-      function () {
-        resolve(this.lastID);
-      }
-    );
-  });
-};
-
-const selectAllRecordsPromise = () => {
-  return new Promise((resolve) => {
-    db.all("SELECT * FROM books", (_, rows) => {
+    db.all(sql, param, (_, rows) => {
       resolve(rows);
     });
   });
 };
 
-const closeDbPromise = () => {
+const dbClosePromise = function () {
   return new Promise((resolve) => {
-    db.close(() => {
+    db.close(function () {
       resolve();
     });
   });
 };
 
 async function main() {
-  await createTablePromise();
-  console.log("Created books table successfully.");
+  await dbRunPromise(
+    "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+  );
+  console.log("Create table successfully.");
 
-  const lastID = await insertRecordsPromise();
-  console.log("Inserted records successfully.");
-  console.log(`The inserted last ID is ${lastID}`);
+  const lastId = await dbRunPromise(
+    "INSERT INTO books (title) VALUES ($title1), ($title2)",
+    {
+      $title1: "FirstBook",
+      $title2: "SecondBook",
+    }
+  );
+  console.log(`The last inserted ID is ${lastId}`);
 
-  const rows = await selectAllRecordsPromise();
-  console.log("The below allay means all records.");
+  const rows = await dbAllPromise("SELECT * FROM books");
   console.log(rows);
 
-  await closeDbPromise();
+  await dbClosePromise();
   console.log("Closed DB successfully.");
 }
 
