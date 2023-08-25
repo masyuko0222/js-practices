89c62479f03d52c85ad4cd94a8f63bcde5ab8e32
masyuko0222 <masyuko0222@gmail.com>
2023-08-25 22:45:45 +0900

    Promise OK版

--- 03.asynchronous/promise/success_promise.js
+++ 03.asynchronous/promise/success_promise.js
@@ -4,62 +4,70 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-const createTablePromise = () => {
+const dbRunPromise = (sql, param = {}, cb = new Function()) => {
   return new Promise((resolve) => {
-    db.run(
-      "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-      () => {
-        resolve();
+    db.run(sql, param, function () {
+      if (this.lastID) {
+        const lastId = this.lastID;
+        cb(lastId);
+        return resolve();
       }
-    );
-  });
-};
 
-const insertRecordsPromise = () => {
-  return new Promise((resolve) => {
-    db.run(
-      "INSERT INTO books (title) VALUES ($title1), ($title2)",
-      { $title1: "FirstBook", $title2: "SecondBook" },
-      function () {
-        resolve(this.lastID);
-      }
-    );
-  });
-};
-
-const selectAllRecordsPromise = () => {
-  return new Promise((resolve) => {
-    db.all("SELECT * FROM books", (_, rows) => {
-      resolve(rows);
-    });
-  });
-};
-
-const closeDbPromise = () => {
-  return new Promise((resolve) => {
-    db.close(() => {
+      cb();
       resolve();
     });
   });
 };
 
-createTablePromise()
-  .then(() => {
-    console.log("Created books table successfully.");
-    return insertRecordsPromise();
-  })
-  .then((lastId) => {
-    console.log("Inserted records successfully.");
-    console.log(`The inserted last ID is ${lastId}`);
-    return selectAllRecordsPromise();
-  })
-  .then((rows) => {
-    console.log("The below allay means all records.");
-    console.log(rows);
-  })
-  .finally(() => {
-    return closeDbPromise();
-  })
-  .then(() => {
-    console.log("Closed DB successfully.");
+const dbAllPromise = (sql, param = {}, cb = new Function()) => {
+  return new Promise((resolve) => {
+    db.all(sql, param, function (_, rows) {
+      if (rows) {
+        cb(_, rows);
+        return resolve();
+      }
+      cb();
+      resolve();
+    });
   });
+};
+
+const dbClosePromise = function (cb = new Function()) {
+  return new Promise((resolve) => {
+    db.close(function () {
+      cb();
+      resolve();
+    });
+  });
+};
+
+function main() {
+  dbRunPromise(
+    "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    {},
+    () => {
+      console.log("Created table successfully.");
+    }
+  )
+    .then(() => {
+      return dbRunPromise(
+        "INSERT INTO books (title) VALUES ($title1), ($title2)",
+        { $title1: "FirstBook", $title2: "SecondBook" },
+        function (lastId) {
+          console.log(`The last inserted ID is ${lastId}`);
+        }
+      );
+    })
+    .then(() => {
+      return dbAllPromise("SELECT * FROM books", {}, (_, rows) => {
+        console.log(rows);
+      });
+    })
+    .finally(() => {
+      dbClosePromise(() => {
+        console.log("Closed DB successfully.");
+      });
+    });
+}
+
+main();
