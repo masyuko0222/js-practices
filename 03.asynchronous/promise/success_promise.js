17012afc910bb6ea2017924de2270637cd702f2c
masyuko0222 <masyuko0222@gmail.com>
2023-08-25 23:39:38 +0900

    Promise OK版リファクタリング

--- 03.asynchronous/promise/success_promise.js
+++ 03.asynchronous/promise/success_promise.js
@@ -4,30 +4,18 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-const dbRunPromise = (sql, param = {}, cb = new Function()) => {
+const dbRunPromise = (sql, param = {}) => {
   return new Promise((resolve) => {
     db.run(sql, param, function () {
-      if (this.lastID) {
-        const lastId = this.lastID;
-        cb(lastId);
-        return resolve();
-      }
-
-      cb();
-      resolve();
+      resolve(this.lastID);
     });
   });
 };
 
-const dbAllPromise = (sql, param = {}, cb = new Function()) => {
+const dbAllPromise = (sql, param = {}) => {
   return new Promise((resolve) => {
-    db.all(sql, param, function (_, rows) {
-      if (rows) {
-        cb(_, rows);
-        return resolve();
-      }
-      cb();
-      resolve();
+    db.all(sql, param, (_, rows) => {
+      resolve(rows);
     });
   });
 };
@@ -43,25 +31,28 @@ const dbClosePromise = function (cb = new Function()) {
 
 function main() {
   dbRunPromise(
-    "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    {},
-    () => {
-      console.log("Created table successfully.");
-    }
+    "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
   )
+    .then(() => {
+      console.log("Created table successfully.");
+    })
     .then(() => {
       return dbRunPromise(
         "INSERT INTO books (title) VALUES ($title1), ($title2)",
-        { $title1: "FirstBook", $title2: "SecondBook" },
-        function (lastId) {
-          console.log(`The last inserted ID is ${lastId}`);
+        {
+          $title1: "FirstBook",
+          $title2: "SecondBook",
         }
       );
     })
+    .then((lastId) => {
+      console.log(`The last inserted ID is ${lastId}`);
+    })
     .then(() => {
-      return dbAllPromise("SELECT * FROM books", {}, (_, rows) => {
-        console.log(rows);
-      });
+      return dbAllPromise("SELECT * FROM books");
+    })
+    .then((rows) => {
+      console.log(rows);
     })
     .finally(() => {
       dbClosePromise(() => {
