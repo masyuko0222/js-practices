5fb1573f5e99d2eac6ab13a623c656118a334899
masyuko0222 <masyuko0222@gmail.com>
2023-09-01 20:37:11 +0900

    新規: コミット修正: メモ一覧はこちらで初実装

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,19 +1,41 @@
-import pkg from "enquire";
+import enquirer from "enquirer";
 import fs from "fs";
 import sqlite3 from "sqlite3";
 import minimist from "minimist";
 
-const enquire = pkg;
-
 function main() {
-  save();
+  const args = minimist(process.argv);
+
+  if (args.l) {
+    index();
+  } else {
+    save();
+  }
+}
+
+//* Controller *//
+async function index() {
+  const db = new sqlite3.Database("memos.db");
+  const select = "SELECT * FROM memos";
+
+  try {
+    const memos = await all(db, select);
+    memos.forEach((memo) => render(memo.content));
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(err);
+    } else {
+      throw err;
+    }
+  } finally {
+    await close(db);
+  }
 }
 
 async function save() {
   const db = new sqlite3.Database("memos.db");
   const stdin = fs.readFileSync("/dev/stdin", "utf8");
 
-  // SQL
   const createTable =
     "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL)";
   const insertMemo = "INSERT INTO memos(content) values(?)";
@@ -32,10 +54,24 @@ async function save() {
   }
 }
 
+//* View *//
+function render(content) {
+  console.log(content.split(`\n`)[0].trim());
+}
+
+//* SQL functions *//
 function run(db, sql, params = []) {
   return new Promise((resolve, reject) => {
     db.run(sql, params, function (err) {
       err ? reject(err) : resolve(this.lastID);
+    });
+  });
+}
+
+function all(db, sql, params = {}) {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      err ? reject(err) : resolve(rows);
     });
   });
 }
