5aa8042d710593df802c360422cdb468fddc17c5
masyuko0222 <masyuko0222@gmail.com>
2023-09-04 21:29:30 +0900

    新規: メモを参照する機能

--- 04.class/memo_app.js
+++ 04.class/memo_app.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 
+import enquirer from "enquirer";
 import fs from "fs";
 import minimist from "minimist";
 import sqlite3 from "sqlite3";
@@ -10,25 +11,43 @@ async function main() {
   const options = minimist(process.argv);
   const db = new sqlite3.Database(DB);
 
-  if (options.l) {
-    index(db);
-  } else {
-    const memo = newMemo();
-    save(db, memo);
+  try {
+    if (options.l) {
+      index(db);
+    } else if (options.r) {
+      show(db);
+    } else {
+      save(db);
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+    } else {
+      throw err;
+    }
   }
 }
 
+// Controller
 async function index(db) {
-  const memos = await all(db, "SELECT * FROM memos");
+  const memos = await allMemos(db);
 
   memos.forEach((memo) => {
     console.log(memo.firstLine);
   });
-
   await close(db);
 }
 
-async function save(db, memo) {
+async function show(db) {
+  const memos = await allMemos(db);
+  const memo = await selectMemo(memos);
+  console.log(memo);
+  await close(db);
+}
+
+async function save(db) {
+  const memo = newMemo();
+
   await run(
     db,
     "CREATE TABLE IF NOT EXISTS memos(id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL, firstLine TEXT NOT NULL)"
@@ -42,12 +61,34 @@ async function save(db, memo) {
   await close(db);
 }
 
-function newMemo() {
+const newMemo = () => {
   const stdin = fs.readFileSync("/dev/stdin", "utf8");
   const firstLine = stdin.split(`\n`)[0].trim();
   return { content: stdin, firstLine: firstLine };
-}
+};
 
+const allMemos = async (db) => {
+  return all(db, "SELECT * FROM memos");
+};
+
+const selectMemo = async (memos) => {
+  const question = {
+    type: "select",
+    name: "memos",
+    message: "Choose a note you want to see:",
+    choices: memos.map((memo) => {
+      return { name: memo.firstLine, value: memo.content };
+    }),
+    result() {
+      return this.focused.value;
+    },
+  };
+
+  const answer = await enquirer.prompt(question);
+  return answer.memos;
+};
+
+// db functions
 function run(db, sql, params = {}) {
   return new Promise((resolve, reject) => {
     db.run(sql, params, (err) => {
